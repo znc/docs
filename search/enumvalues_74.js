0242684769cede4f189ad38ef14a45031f85b484
travis-ci <travis@travis-ci.org>
2014-04-24 18:46:42 +0000

    Latest docs on successful travis build 457
    ZNC commit ed7119b5bc9699ee54376f2851a1ffaa31912cda

--- search/enumvalues_74.js
+++ search/enumvalues_74.js
@@ -1,8 +1,8 @@
 var searchData=
 [
   ['tls1',['TLS1',['../classCsock.html#aa7da8fe2945d8cfca7c53aec88fdad68aa5f0d66c54b5d1d1dc470a8de2196f3b',1,'Csock']]],
-  ['tls1_5f1',['TLS1_1',['../classCsock.html#aa7da8fe2945d8cfca7c53aec88fdad68af3d01e165f7f777827617031c694f3f2',1,'Csock']]],
-  ['tls1_5f2',['TLS1_2',['../classCsock.html#aa7da8fe2945d8cfca7c53aec88fdad68a5965386a3a14369f5a0be8b5e4decee8',1,'Csock']]],
+  ['tls11',['TLS11',['../classCsock.html#aa7da8fe2945d8cfca7c53aec88fdad68afe7b977cb013e4e4cecf52c5e556685e',1,'Csock']]],
+  ['tls12',['TLS12',['../classCsock.html#aa7da8fe2945d8cfca7c53aec88fdad68af4dceb7a8d0001e838430f7cbed6b9cb',1,'Csock']]],
   ['tmo_5faccept',['TMO_ACCEPT',['../classCsock.html#a02fc87b0c2dceedeaa8df5407b389f6aa7b49e7c8277b2b9f541a370d9c3e656d',1,'Csock']]],
   ['tmo_5fall',['TMO_ALL',['../classCsock.html#a02fc87b0c2dceedeaa8df5407b389f6aaf53c83452a73e97d1b89e9e7743ddb13',1,'Csock']]],
   ['tmo_5fread',['TMO_READ',['../classCsock.html#a02fc87b0c2dceedeaa8df5407b389f6aa7ca5c1837fd4777f4c30736ba3d2b6b2',1,'Csock']]],
