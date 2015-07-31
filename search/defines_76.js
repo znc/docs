884d4aa88ed0e37f5cfd1624d525ff592138f19a
znc-travis <travis-ci@znc.in>
2015-07-31 22:16:43 +0000

    Latest docs on successful travis build 1406
    ZNC commit 015a8f6502bd5f0771255fedd5f981b7c99f1d16

--- search/defines_76.js
+++ search/defines_76.js
@@ -1,6 +1,7 @@
 var searchData=
 [
   ['version',['VERSION',['../version_8h.html#a1c6d5de492ac61ad29aec7aa9a436bbf',1,'version.h']]],
+  ['version_5fextra',['VERSION_EXTRA',['../version_8h.html#a078eaa4fbbcc2c248521462f87787e87',1,'version.h']]],
   ['version_5fmajor',['VERSION_MAJOR',['../version_8h.html#a1a53b724b6de666faa8a9e0d06d1055f',1,'version.h']]],
   ['version_5fminor',['VERSION_MINOR',['../version_8h.html#ae0cb52afb79b185b1bf82c7e235f682b',1,'version.h']]],
   ['version_5fpatch',['VERSION_PATCH',['../version_8h.html#a901edadf17488bb6be1ac9a1e3cfea7a',1,'version.h']]],
