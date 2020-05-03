e5c2df1d859715349319f4e109910dd14082efbb
znc-travis <travis-ci@znc.in>
2020-05-03 08:16:38 +0000

    Latest docs on successful travis build 3349
    ZNC commit 7578d366d0eecd4bbb948ccf5814d9ca368028fd

--- search/all_16.js
+++ search/all_16.js
@@ -10,10 +10,6 @@ var searchData=
   ['version',['VERSION',['../version_8h.html#a1c6d5de492ac61ad29aec7aa9a436bbf',1,'version.h']]],
   ['version_2eh',['version.h',['../version_8h.html',1,'']]],
   ['version_5fextra',['VERSION_EXTRA',['../version_8h.html#a078eaa4fbbcc2c248521462f87787e87',1,'version.h']]],
-  ['version_5fmajor',['VERSION_MAJOR',['../version_8h.html#a1a53b724b6de666faa8a9e0d06d1055f',1,'version.h']]],
-  ['version_5fminor',['VERSION_MINOR',['../version_8h.html#ae0cb52afb79b185b1bf82c7e235f682b',1,'version.h']]],
-  ['version_5fpatch',['VERSION_PATCH',['../version_8h.html#a901edadf17488bb6be1ac9a1e3cfea7a',1,'version.h']]],
-  ['version_5fstr',['VERSION_STR',['../version_8h.html#acfc1668731750cb1ee8974b8a7c133ef',1,'version.h']]],
   ['voice',['Voice',['../classCChan.html#a8e48d2ab8131ea73f085d4dfb0591f16a4a787e9ee2b7138f332e33e0abb64908',1,'CChan']]],
   ['vpair',['VPair',['../ZNCString_8h.html#a293a93c83fd11c40659f9bcb9f21419f',1,'ZNCString.h']]],
   ['vwebsubpages',['VWebSubPages',['../WebModules_8h.html#ab585ce11740587b08deb0da25de3c2d6',1,'WebModules.h']]]
