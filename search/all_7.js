5b2333c17b41c8933c8b6b1a034e07e828910a35
znc-travis <travis-ci@znc.in>
2025-05-11 20:27:12 +0000

    Latest docs on successful CI build 444
    ZNC commit 694f50c9c0c0ae1fd77f86528027f14984ee2e57

--- search/all_7.js
+++ search/all_7.js
@@ -307,7 +307,7 @@ var searchData=
   ['getuserclients_304',['GetUserClients',['../classCUser.html#ae2f17edcd0d7438d3b7d28d7729b78f6',1,'CUser']]],
   ['getusermap_305',['GetUserMap',['../classCZNC.html#a918c294b5265a6d1b0e448210960fd40',1,'CZNC']]],
   ['getusermodes_306',['GetUserModes',['../classCIRCSock.html#a4d9a979bfba40bf01c8d9c66511d1897',1,'CIRCSock']]],
-  ['getusername_307',['getusername',['../classCAuthBase.html#a3c960dabfa3b855cd5a80db8c6e166ed',1,'CAuthBase::GetUsername()'],['../classCUser.html#a802466aded0dd09478069f4240640129',1,'CUser::GetUsername() const'],['../classCUser.html#a1d13a858742c8a3da921695530baaeb2',1,'CUser::GetUserName() const']]],
+  ['getusername_307',['getusername',['../classCUser.html#a802466aded0dd09478069f4240640129',1,'CUser::GetUsername() const'],['../classCUser.html#a1d13a858742c8a3da921695530baaeb2',1,'CUser::GetUserName() const'],['../classCAuthBase.html#a3c960dabfa3b855cd5a80db8c6e166ed',1,'CAuthBase::GetUsername()']]],
   ['getuserpath_308',['getuserpath',['../classCUser.html#a155c694aeb3c6123b737d38cdaca93aa',1,'CUser::GetUserPath()'],['../classCZNC.html#af27e556b21bf39b3a6107f38cc066f8c',1,'CZNC::GetUserPath()']]],
   ['getvalue_309',['getvalue',['../classCTemplateLoopContext.html#af759a07a3f543ce03340b78520f35cf6',1,'CTemplateLoopContext::GetValue()'],['../classCTemplate.html#aea3aa5598fecc8d7635dcdeb98710b20',1,'CTemplate::GetValue()']]],
   ['getversion_310',['GetVersion',['../classCZNC.html#a928920d8b4902ff1e8c85915eaa8ab61',1,'CZNC']]],
