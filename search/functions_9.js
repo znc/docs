ccf61049c6d12a9e91134ff85e2f8a7de7c444be
znc-travis <travis-ci@znc.in>
2026-08-17 07:31:21 +0000

    Latest docs on successful CI build 685
    ZNC commit 836379a5672f922f5913e9300c59508f0537a9df

--- search/functions_9.js
+++ search/functions_9.js
@@ -74,7 +74,7 @@ var searchData=
   ['isunixsocket_71',['IsUnixSocket',['../classCServer.html#ac38a88e1d5733c4bf0f13d6928abdaa2',1,'CServer']]],
   ['isuserattached_72',['isuserattached',['../classCIRCNetwork.html#a1af1a26176f446054c1febf55929db43',1,'CIRCNetwork::IsUserAttached()'],['../classCUser.html#ad1502aa16e310481ebf0c6f2c4822828',1,'CUser::IsUserAttached()']]],
   ['isuseronline_73',['IsUserOnline',['../classCIRCNetwork.html#a3ad99e4a8196b05116c07dcfc3601652',1,'CIRCNetwork']]],
-  ['isvalid_74',['isvalid',['../classCUser.html#a3de3189c2088f0de4898162b62368b5d',1,'CUser::IsValid()'],['../classCCron.html#aea03cbadbf195c0ce6f035c06557eef6',1,'CCron::isValid()']]],
+  ['isvalid_74',['isvalid',['../classCCron.html#aea03cbadbf195c0ce6f035c06557eef6',1,'CCron::isValid()'],['../classCUser.html#a3de3189c2088f0de4898162b62368b5d',1,'CUser::IsValid()']]],
   ['isvalidheaderfield_75',['IsValidHeaderField',['../classCHTTPSock.html#a6cd159af9b3b4a5a9c6e4f38f1c04602',1,'CHTTPSock']]],
   ['isvalidhostname_76',['IsValidHostName',['../classCServer.html#a90810ca5e292e219244a800ad8e7cee0',1,'CServer']]],
   ['isvalididentifier_77',['IsValidIdentifier',['../classCClient.html#a25e8c5f0e58ba2b8132d073576a295ef',1,'CClient']]],
